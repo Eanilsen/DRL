8ed7624d9f6574cb71fca6d74cfa742d2d0ac98d
eanilsen <evenanilsen@gmail.com>
2015-01-28 02:29:03 +0100

    Graphics module

--- core/game.py
+++ core/game.py
@@ -1,8 +1,7 @@
-import curses
+import graphics as gfx
 import sys
 import traceback
-
-stdscr = None
+import curses
 
 # A Game represents a single instance of a game, including its maps,
 # data and everything else.
@@ -14,7 +13,7 @@ class Game(object):
         running = True
         x,y = 0,0
         while running:
-            c = stdscr.getch()
+            c = gfx.scr().getch()
             if c == curses.KEY_UP: y -= 1
             elif c == curses.KEY_DOWN: y += 1
             elif c == curses.KEY_LEFT: x -= 1
@@ -22,30 +21,16 @@ class Game(object):
             elif c == ord('q'): running = False
 
             if c != -1:
-                stdscr.clear()
-                stdscr.addch(y, x, '@')
+                gfx.scr().clear()
+                gfx.scr().addch(y, x, '@')
 
     # Runs an interactive session of our game with the player.
     def play(self):
-        global stdscr
-
-        stdscr = curses.initscr()
-        curses.noecho()
-        curses.cbreak()
-        curses.curs_set(0)
-        stdscr.keypad(1)
-        stdscr.timeout(0)
-        stdscr.nodelay(1)
+        gfx.start()
 
         try:
             self.step()
         except:
-            curses.nocbreak()
-            stdscr.timeout(-1)
-            stdscr.keypad(0)
-            curses.echo()
-            curses.endwin()
-
+            gfx.stop()
             print traceback.format_exc()
-
             sys.exit(-1)
