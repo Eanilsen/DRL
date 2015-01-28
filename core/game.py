8ffd93f9554ba87e0a2f65dd149cab30c5287e4f
eanilsen <evenanilsen@gmail.com>
2015-01-28 15:37:14 +0100

    Removed handle_keys()

--- core/game.py
+++ core/game.py
@@ -4,39 +4,34 @@
 # graphics.
 
 from core import graphics
+from core import world
 
 import sys
 import traceback
-import curses
 
 # A Game represents a single instance of a game, including its maps,
 # data, and everything else.
 class Game(object):
     def __init__(self):
-        self.x, self.y = 0, 0
+        self.world = world.World()
 
-    def step(self):
-        running = True
-        x,y = 0,0
-        while running:
-            c = graphics.scr().getch()
-            if c == curses.KEY_UP: y -= 1
-            elif c == curses.KEY_DOWN: y += 1
-            elif c == curses.KEY_LEFT: x -= 1
-            elif c == curses.KEY_RIGHT: x += 1
-            elif c == ord('q'): running = False
-
-            if c != -1:
-                graphics.scr().clear()
-                graphics.scr().addch(y, x, '@')
-
-    # Runs an interactive session of our game with the player.
+    # Runs an interactive session of our game with the player until either
+    # the player stops playing or an error occurs. Here, we pass input to the
+    # world until we are told we do not need to anymore. If an error occurs, we
+    # turn off the graphics, print the traceback, and kill the program.
     def play(self):
         graphics.start()
 
         try:
-            self.step()
+            running = True
+            while running:
+                c = graphics.get_input()
+                self.world.handle_keys(c)
+                if c == 'q': running = False
+                self.world.draw()
         except:
             graphics.stop()
             print traceback.format_exc()
             sys.exit(-1)
+
+        graphics.stop()
