f68b0220d1d4ccf0d63a34a70243e2e9ae019e24
eanilsen <evenanilsen@gmail.com>
2015-01-28 03:00:46 +0100

    Updated graphics.py

--- core/game.py
+++ core/game.py
@@ -1,19 +1,25 @@
-import graphics as gfx
+# The Game is where it all starts. A Game is an abstract and thin package in
+# which all of the elements of the game are stored. It is responsible for
+# creating the world, parsing and writing to save files, and turning on/off
+# graphics.
+
+from core import graphics
+
 import sys
 import traceback
 import curses
 
 # A Game represents a single instance of a game, including its maps,
-# data and everything else.
+# data, and everything else.
 class Game(object):
     def __init__(self):
-        pass
+        self.x, self.y = 0, 0
 
     def step(self):
         running = True
         x,y = 0,0
         while running:
-            c = gfx.scr().getch()
+            c = graphics.scr().getch()
             if c == curses.KEY_UP: y -= 1
             elif c == curses.KEY_DOWN: y += 1
             elif c == curses.KEY_LEFT: x -= 1
@@ -21,16 +27,16 @@ class Game(object):
             elif c == ord('q'): running = False
 
             if c != -1:
-                gfx.scr().clear()
-                gfx.scr().addch(y, x, '@')
+                graphics.scr().clear()
+                graphics.scr().addch(y, x, '@')
 
     # Runs an interactive session of our game with the player.
     def play(self):
-        gfx.start()
+        graphics.start()
 
         try:
             self.step()
         except:
-            gfx.stop()
+            graphics.stop()
             print traceback.format_exc()
             sys.exit(-1)
